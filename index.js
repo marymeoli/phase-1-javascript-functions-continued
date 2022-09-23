50cc40071be1e2d212434a77c7adf2aa1f0d9436
Moringa School <marymeoli77@gmail.com>
2022-09-23 11:14:52 +0300

    submit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    console.log(`This saturday, I want to ${activity}!`)
+    return `This Saturday, I want to ${activity}!`
+}
+// saturdayFun()
+
+function mondayWork (doWhat = 'go to the office') {
+    console.log(`This Monday, I will ${doWhat}.`);
+    return `This Monday, I will ${doWhat}.`
+}
+function wrapAdjective (part1="*") {
+    //const encouragingPromptFuntion =  wrapAdjective("!!!")
+    return function(emphatic="special"){
+        return`You are ${part1}${emphatic}${part1}!`
+    }
+}
